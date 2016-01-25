54005e05eddc5dad678ca72e7e7511ec81292cf7
czerkies <roman.czerkies@gmail.com>
2016-01-25 23:09:23 +0100

    Feat(template): CSS et html
    Mise en place css pour les parametres
    Bouton callto
    Mobile first
    
    Issue #2

--- vues/template.php
+++ vues/template.php
@@ -8,24 +8,36 @@
   </head>
   <body>
     <header>
+      <img src="" alt="Logo Le petit saint bernard">
       <h1>Le petit saint bernard</h1>
     </header>
     <div id="cont">
+      <div class="callto">
+        <a href="#" class="button w50">Test</a>
+      </div>
       <h2>Le petit Saint Bernard ?</h2>
-      <p>Le petit saint bernard est un spécialiste de la configuration personalisé en vélo de montagne.</p>
+      <p>Le petit saint bernard est un spécialiste de la configuration personalisé en vélo de montagne.<br>Notre magasin et usine se situe en Rhon-Alpes dans la ville de Bourg Saint Maurice. Notre Fabrication, votre configuration,</p>
       <h2>Trouver son vélo</h2>
-      <p>Pour trouver son vélo parfait, rien de plus simple, créer votre profil, nous vous proposerons les vélo qui vous conviendrait le mieux.<br>Nous prenoms en compte les critères suivant :</p>
+      <p>Pour trouver son vélo parfait, rien de plus simple, après avoir créé votre profil, nous vous proposerons les vélos qui vous conviendrait le mieux.<br>Nous prenons en compte les critères suivant :</p>
       <div class="spe">
-        <div class="round" id="sexe">Sexe</div>
-        <div class="round" id="taille">Taille</div>
-        <div class="round" id="poid">Poid</div>
-        <div class="round" id="age">Age</div>
-        <div class="round" id="style">Route/VTT</div>
+        <div class="round" id="sexe"><span>Sexe</span></div>
+        <div class="round" id="taille"><span>Taille</span></div>
+        <div class="round" id="poid"><span>Poid</span></div>
+        <div class="round" id="age"><span>Age</span></div>
+        <div class="round" id="type"><span>Type</span></div>
+        <div class="round" id="budget"><span>Budget</span></div>
       </div>
       <div class="infos">
+        <div id="infos_prin" class="hidden appear"><span>Les 6 parametres définirons votre vélo idéal.</span><br>Vous pouvez cliquer sur chacun parametres pour en savoir plus.</div>
         <div id="infos_sexe" class="hidden">Cette information nous donne accès au format du cadre et au type de selle.</div>
         <div id="infos_taille" class="hidden">Cette information nous donne accès à la taille du cadre et des roues.</div>
+        <div id="infos_poid" class="hidden">Cette information nous donne accès à la taille du cadre et des roues.</div>
+        <div id="infos_age" class="hidden">Cette information nous donne accès à la taille du cadre et des roues.</div>
+        <div id="infos_type" class="hidden">Cette information nous donne accès à la taille du cadre et des roues.</div>
+        <div id="infos_budget" class="hidden">Cette information nous donne accès à la taille du cadre et des roues.</div>
       </div>
+      <h2>Garantie et support</h2>
+      <p>Chaque vélo est garantie d'une durée de 3ans, notre support technique et commercial est à votre disposition.<br>Vous pouvez nous contacter à tout moment, par notre formulaire de contact ou par téléphone au numéro suivant : 01 02 03 04 05.</p>
     </div>
   </body>
   <script src="../www/js/jquery.js"></script>
