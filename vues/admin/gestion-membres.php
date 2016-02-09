d00fa97ef639fc23f4e6e129a12a076698170f83
czerkies <roman.czerkies@gmail.com>
2016-02-09 21:25:16 +0100

    Feat(gestion-membres): Format des données
    CSS sur les l'affichage des données
    
    Issue #5

--- vues/admin/gestion-membres.php
+++ vues/admin/gestion-membres.php
@@ -1,10 +1,24 @@
 <?php if($userConnectAdmin){ ?>
 <h2>Gestion des membres</h2>
-
-<?php if($listeMembres){
-  foreach ($listeMembres as $value) {
-    echo $value['email'].'<br>';
-  }
-} ?>
-
+<p>Gérer les membres. Supprimer un membre. Ajouter un administrateur.</p>
+<?php if($listeMembres){ ?>
+  <div class="tableau">
+    <div class="head">
+      <div class="cel w5">ID</div>
+      <div class="cel w20">Email</div>
+      <div class="cel w20">Prénom</div>
+    </div>
+    <ul class="body">
+      <?php foreach($listeMembres as $value) { ?>
+        <li>
+          <span class="w5"><?= $value['id_membre']; ?></span>
+          <span class="w20"><?= $value['email']; ?></span>
+          <span class="w20"><?= $value['prenom']; ?></span>
+        </li>
+      <?php } ?>
+    </ul>
+  </div>
+<?php } else { ?>
+  <p>Aucun membre trouvés</p>
 <?php } ?>
+<?php }
