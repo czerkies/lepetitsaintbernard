db5cecb9241851ac5be0fd94488b18b933d90b01
roman <roman.czerkies@gmail.com>
2016-02-24 08:38:49 +0100

    Fix(votre-velo): Debug session
    Verification si session exist
    
    Issue #8

--- controleurs/controleurAssemblage.php
+++ controleurs/controleurAssemblage.php
@@ -23,7 +23,7 @@ class controleurAssemblage extends controleurSuper {
 
     $velos = new modeleAssemblage();
 
-    $veloPerso = $velos->veloAssemblage($_SESSION['membre']);
+    $veloPerso = (isset($_SESSION['membre'])) ? $velos->veloAssemblage($_SESSION['membre']) : null;
 
     $this->Render('../vues/velo/velo-perso.php', array('meta' => $meta, 'msg' => $msg, 'userConnect' => $userConnect, 'userConnectAdmin' => $userConnectAdmin, 'veloPerso' => $veloPerso));
 
