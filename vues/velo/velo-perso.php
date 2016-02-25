548f9fc784f5324be4ac0b288385d7ea47e3b34b
roman <roman.czerkies@gmail.com>
2016-02-25 22:27:42 +0100

    Feat(votre-velo): Gestion si données vides
    CSS sur les carte piece.
    Gestion si données vides.
    
    Issue #8

--- vues/velo/velo-perso.php
+++ vues/velo/velo-perso.php
@@ -12,37 +12,48 @@ if($userConnect){
   <?php
   $prix[$key] = 0;
   $poids[$key] = 0;
-  foreach ($typeVelo as $donnees) { ?>
+  foreach ($typeVelo as $type => $donnees) { ?>
     <div class="blocpiece">
-      <div class="img_titre_piece">
-        <p class="titre_piece"><?= $donnees['titre']; ?></p>
+      <?php if(!empty($donnees)) { ?>
+      <div class="img_piece">
+        <p class="type_piece"><?= ucfirst($type); ?></p>
         <img src="<?= RACINE_SITE.$donnees['img']; ?>" alt="<?= $donnees['titre']; ?>">
       </div>
-      <div class="description_piece">
-        <p><?= $donnees['description']; ?></p>
+      <div class="details_piece">
+        <p class="titre"><?= $donnees['titre']; ?></p>
+        <p class="description"><?= $donnees['description']; ?></p>
+        <?php if(($donnees['pignon'] && $donnees['plateau']) == null) { ?>
+          <p><b>Matière</b> : <?= ucfirst($donnees['matiere']); ?></p>
+          <?php } else { ?>
+            <p><b>Groupe</b> : <?= $donnees['plateau'].'/'.$donnees['pignon']; ?></p>
+            <?php } ?>
         <p><b>Poids</b> : <?= $donnees['poids']; ?> Kilos</p>
         <p><b>Prix</b> : <?= $donnees['prix']; ?> €</p>
-        <?php if(($donnees['pignon'] && $donnees['plateau']) == null) { ?>
-          <p><b>Matière</b> : <?= $donnees['matiere']; ?></p>
-        <?php } else { ?>
-          <p><b>Groupe</b> : <?= $donnees['plateau'].'/'.$donnees['pignon']; ?></p>
-        <?php } ?>
       </div>
+      <?php } else { ?>
+        <div class="details_piece">
+          <p class="description">
+            Aucune pièce de type <?= $type; ?> n'a été trouvé.<br>
+            Veuillez configurer votre vélo ou faire une demande de pièce via la page <a href="<?= RACINE_SITE; ?>contact/">contact.</a>
+          </p>
+        </div>
+      <?php } ?>
     </div>
   <?php
     $prix[$key] += $donnees['prix'];
     $poids[$key] += $donnees['poids'];
   }
   ?>
-  <div class="blocpiece">
-    <div class="description_piece">
-      <p><b>Prix du vélo</b> : <?= $prix[$key]; ?> €.</p>
-      <p><b>Poids du vélo</b> : <?= $poids[$key]; ?> Kilos.</p>
+  <?php if(!array_search(null, $typeVelo)) { ?>
+    <div class="blocpiece">
+      <div class="details_piece">
+        <p><b>Prix du vélo</b> : <?= $prix[$key]; ?> €.</p>
+        <p><b>Poids du vélo</b> : <?= $poids[$key]; ?> Kilos.</p>
+      </div>
     </div>
-  </div>
+    <?php } ?>
 
 <?php } ?>
 
 <?php
 }
-?>
