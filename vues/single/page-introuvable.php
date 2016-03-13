100b41576414892f9cf197534a7c832f769b4eb0
roman <roman.czerkies@gmail.com>
2016-03-13 22:22:58 +0100

    Feat(mvc): Debug
    Debug css et 404.
    
    Issue #24

--- vues/single/page-introuvable.php
+++ vues/single/page-introuvable.php
@@ -1,3 +1,3 @@
 <h2>Page introuvable</h2>
 <p>Désolé, votre page est introuvable.</p>
-<p><a href="<?= RACINE_SITE; ?>">Lokisalle vous permet de louer des salles</a></p>
+<p><a href="<?= RACINE_SITE; ?>">Le petit St Bernard vous permet de commander votre vélo, sur mesure.</a></p>
