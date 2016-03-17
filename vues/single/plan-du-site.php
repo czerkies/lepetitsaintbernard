9722726d64abdda304e61b7db521a59f09b36677
czerkies <roman.czerkies@gmail.com>
2016-03-17 08:59:45 +0100

    Feat(plan-du-site): Ajout content
    Ajout plan du site dynamique.
    
    Issue #31

--- vues/single/plan-du-site.php
+++ vues/single/plan-du-site.php
@@ -1,3 +1,47 @@
 <h2>Plan du site</h2>
 
-<p>Toutes les pages</p>
+<p>
+  <b>Menu</b>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>/">Accueil</a>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>votre-velo/">Votre Vélo</a>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>configuration/">Configuration</a>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>panier/">Panier</a>
+</p>
+<?php if(!($userConnect || $userConnectAdmin)){ ?>
+  <p>
+    <a href="<?= RACINE_SITE; ?>connexion/">Se connecter</a>
+  </p>
+  <p>
+    <a href="<?= RACINE_SITE; ?>creation-compte/">Créer son compte</a>
+  </p>
+<?php } if($userConnect || $userConnectAdmin){ ?>
+  <p>
+    <a href="<?= RACINE_SITE; ?>mon-compte/">Mon compte</a>
+  </p>
+  <p>
+    <a href="<?= RACINE_SITE; ?>connexion/deconnexion/">Se déconnecter</a>
+  </p>
+<?php } ?>
+<p>
+  <b>Autres</b>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>contact/">Contactez-nous</a>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>catalogue/">Notre catalogue</a>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>cgv/">Conditions générale de ventes</a>
+</p>
+<p>
+  <a href="<?= RACINE_SITE; ?>plan-du-site/">Le Plan du site</a>
+</p>
